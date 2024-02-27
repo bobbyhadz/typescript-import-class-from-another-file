4e63068359e67e1336adf21c6b5afc0925d235c2
bobbyhadz <opmftw@gmail.com>
2024-02-27 16:27:56 +0200

    initial commit

---
+++ src/another-file.ts
@@ -0,0 +1,7 @@
+// 👇️ named export
+export function sum(a: number, b: number) {
+  return a + b;
+}
+
+// 👇️ named export
+export const site = 'bobbyhadz.com';
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// 👇️ named import
+import { sum, site } from './another-file';
+
+console.log(sum(50, 10)); // 👉️ 60
+
+console.log(site); // 👉️ bobbyhadz.com
